de394a0e4610ec641bbb8d094a3628e3355640d8
Chirag <chiragxt1@hotmail.com>
2023-07-25 14:48:03 +0530

    ReadlineSync example

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import openai from "./config/open-ai.js";
+import readlineSync from "readline-sync";
 
 const main = async() => {
-    const chatCompletion = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo',
-        messages: [
-            { role: 'user', content: 'What is the capital of United Arab Emirates?' }
-        ]
-    });
-    console.log(chatCompletion.data.choices[0].message.content);
+    const userName = readlineSync.question("Create a username: ");
+    console.log(`Hey ${userName}`);
+    
+    // const chatCompletion = await openai.createChatCompletion({
+    //     model: 'gpt-3.5-turbo',
+    //     messages: [
+    //         { role: 'user', content: 'What is the capital of United Arab Emirates?' }
+    //     ]
+    // });
+    // console.log(chatCompletion.data.choices[0].message.content);
 }
 
 main();
