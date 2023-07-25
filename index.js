f7244c63d4d8f9d85ed80ef83d3d736ec9a666f9
Chirag <chiragxt1@hotmail.com>
2023-07-25 13:40:06 +0530

    Chat completion example

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
+const main = async() => {
+    const chatCompletion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+            { role: 'user', content: 'What is the capital of United Arab Emirates?' }
+        ]
+    });
+    console.log(chatCompletion.data.choices[0].message.content);
+}
+
+main();
